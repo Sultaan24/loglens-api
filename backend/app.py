a0442475a024d8e75bd847dd3998dfc01b72fc6a
root <root@kali.kali>
2026-04-16 15:56:57 +0530

    fix render port

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -139,4 +140,5 @@ def home():
 # Run App
 # ---------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
